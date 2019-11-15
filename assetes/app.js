247eae11a67c36c81449f6fde8c9ac6f47774c79
hatout01 <hatout1@gmail.com>
2019-11-14 23:19:12 -0800

    worked on html and function that scroll down or up to clicked topic

--- assetes/app.js
+++ assetes/app.js
@@ -1,5 +1,56 @@
 $(document).ready(function () {
 
-    console.log("hello")
+
+
+    $("a").click(function (event) {
+        let nav = event.target.id;
+        console.log(nav)
+        switch (nav) {
+            case (nav = 'about'):
+                $('html, body').animate({
+                    scrollTop: $(".about").offset().top
+                }, 2000);
+                break;
+            case (nav = 'projects'):
+                $('html, body').animate({
+                    scrollTop: $(".projects").offset().top
+                }, 2000);
+                break;
+            case (nav = 'hire'):
+                $('html, body').animate({
+                    scrollTop: $(".hire").offset().top
+                }, 2000);
+                break;
+            case (nav = 'contact'):
+                $('html, body').animate({
+                    scrollTop: $(".contact").offset().top
+                }, 2000);
+                break;
+            case (nav = 'others'):
+                $('html, body').animate({
+                    scrollTop: $(".others").offset().top
+                }, 2000);
+                break;
+        }
+    });
+
+    // console.log(nav)
+
+    // $(document).click(function () {
+
+    //     switch (nav) {
+    //         case (nav === 'hire'):
+    //             $('html, body').animate({
+    //                 scrollTop: $(".hire").offset().top
+    //             }, 2000);
+    //     }
+
+
+    // $("#hire").click(function () {
+    //     $('html, body').animate({
+    //         scrollTop: $(".hire").offset().top
+    //     }, 2000);
+    // });
 
 })
+// })
