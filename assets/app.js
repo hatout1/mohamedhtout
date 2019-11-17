3aee7d4501b36c3d8d5b17948b85aa805cc965b1
hatout01 <hatout1@gmail.com>
2019-11-16 16:31:39 -0800

    added function that sends the data to fire base and another tat brings it back and display it on the web page

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+    // firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyCTIveb-o4xcpNsQX-Zf1NfDCKHlM_zjnA",
+        authDomain: "profile-38836.firebaseapp.com",
+        databaseURL: "https://profile-38836.firebaseio.com",
+        projectId: "profile-38836",
+        storageBucket: "profile-38836.appspot.com",
+        messagingSenderId: "812008865031",
+        appId: "1:812008865031:web:acd4221c14e09629b699a2"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    const db = firebase.database();
+    console.log(db)
 
     $("a").click(function (event) {
         let nav = event.target.id;
@@ -37,25 +52,31 @@ $(document).ready(function () {
         }
     });
 
+    $('.others-btn').on('click', function (e) {
+        e.preventDefault();
 
+        var otherName = $('#name-others-form').val();
+        var otherMessage = $('#message-others-form').val();
 
-    // firebase
+        var newReview = {
+            Name: otherName,
+            Message: otherMessage,
+        }
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyCTIveb-o4xcpNsQX-Zf1NfDCKHlM_zjnA",
-        authDomain: "profile-38836.firebaseapp.com",
-        databaseURL: "https://profile-38836.firebaseio.com",
-        projectId: "profile-38836",
-        storageBucket: "profile-38836.appspot.com",
-        messagingSenderId: "812008865031",
-        appId: "1:812008865031:web:262e46e9c9c765cfb699a2"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+        db.ref().push(newReview);
 
-    const db = firebase.initializeApp(firebaseConfig)
+        $('#name-others-form').val('');
+        $('#message-others-form').val('');
+    })
+    db.ref().on("child_added", function (snap) {
+        var otherNameDisp = snap.val().Name
+        var otherMessageDisp = snap.val().Message
 
+        // function to avoid multi entries of data.
+        $('#name-others-form').empty();
+        $('#messgae-others-form').empty();
 
-
+        $(".others-container > othersDisplay").append("<h1> (" + otherMessageDisp + ")</h1>" + "<h2>" + otherNameDisp + "</h2>")
+    });
 
 })
